0317cfe3b25624b9456dab4f312d892868518dee
RafaelCampos1 <rafael.campos.mendonca@hotmail.com>
2022-03-09 19:19:43 -0300

    docker

---
+++ src/test/java/br/com/escola/service/ServiceStudentTest.java
@@ -0,0 +1,36 @@
+package br.com.escola.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceStudentTest {
+
+    @Test
+    void getStudentByRealId() {
+    }
+
+    @Test
+    void getStudentByEmail() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void getStudents() {
+    }
+
+    @Test
+    void validateStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+
+    @Test
+    void saveStudent() {
+    }
+}
