26a87e2aaea37d35db1f992a6f10cae1e0aded73
Rafael Campos Mendonça <rafael.campos.mendonca@hotmail.com>
2022-03-27 01:01:06 -0300

    adding mongodb

--- src/main/java/com/school/model/Student.java
+++ src/main/java/com/school/model/Student.java
@@ -5,9 +5,10 @@ import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-@Document
+
 @Setter
 @Getter
+@Document(collection = "Student")
 public class Student extends Person{
     private String registration;
     private String schoolClass;
